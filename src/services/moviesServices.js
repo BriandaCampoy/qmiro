bc2fe4c09495dd0bbde4a97321ba0936aaa13431
BriandaCampoy <brianda.campoy@gmail.com>
2023-02-17 16:55:20 -0700

    removing axios to do some tests

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -1,35 +1,52 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { API_KEY } from './config';
 const URL_API = 'https://api.themoviedb.org/3';
-const api = axios.create({
-  baseURL:URL_API,
-  headers:{
-    'Content-Type':'application/json;charset=utf-8'
-  },
-  params:{
-    'api_key':API_KEY,
-  }
-});
+// const api = axios.create({
+//   baseURL:URL_API,
+//   headers:{
+//     'Content-Type':'application/json;charset=utf-8'
+//   },
+//   params:{
+//     'api_key':API_KEY,
+//   }
+// });
 
 export default {
   getTrendingMovies: async () => {
     try {
-      const {data} = await api('/trending/movie/week');
-      // const data = await result.json();
-      return data;
-      
+      const res = await fetch(`${URL_API}/trending/movie/week?api_key=${API_KEY}`)
+      const data = await res.json()
+      console.log(data);
+      return data
     } catch (error) {
-      console.log(error);
+      
     }
+    // try {
+    //   const {data} = await api('/trending/movie/week');
+    //   // const data = await result.json();
+    //   console.log(data);
+    //   return data;
+      
+    // } catch (error) {
+    //   console.log(error);
+    // }
   },
   getCategories: async () => {
     try {
-      const {data} = await api(`/genre/movie/list`);
-      // const data = await result.json();
-      return data;
-      
+      const res = await fetch(`${URL_API}/genre/movie/list?api_key=${API_KEY}`)
+      const data = await res.json()
+      console.log(data);
+      return data
     } catch (error) {
-      console.log(error);
+      
     }
+    // try {
+    //   const {data} = await api(`/genre/movie/list`);
+    //   // const data = await result.json();
+    //   return data;
+      
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 };
